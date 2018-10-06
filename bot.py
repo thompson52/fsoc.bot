2e24cbe2a728da4ab48db132a891b7e4d7e855c5
Thomas <34421202+thompson52@users.noreply.github.com>
2018-10-06 16:37:22 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
-import os
+
 
 bot = commands.Bot(command_prefix='')
 
@@ -57,4 +57,4 @@ async def embed(ctx):
     embed.add_field(name="This is a field", value="no it isn't", inline=True)
     await bot.say(embed=embed)
 
-bot.run(os.getenv('TOKEN'))
+bot.run('TOKEN')
