28048ba72731b6b4fb5538d7f35a07b3688e6545
Thomas <34421202+thompson52@users.noreply.github.com>
2018-10-06 18:03:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,18 +8,19 @@ import time
 import os
 
 bot = commands.Bot(command_prefix='')
+bot.remove_command('help')
 
 print (discord.__version__)
 
 @bot.event
 async def on_ready():
     print ("initializing fscoiety v.1.0")
-    print ("I am running" + bot.user.name)
+    print ("I am running " + bot.user.name)
     print ("With the ID: " + bot.user.id)
 
 @bot.command(pass_context=True)
 async def ping(ctx):
-    await bot.say(":Pinged.")
+    await bot.say("192.251.68.253")
     print ("user has pinged")
 
 @bot.command(pass_context=True)
@@ -54,7 +55,17 @@ async def botinfo(ctx):
     embed = discord.Embed(title="Made with:", description="Python 3.7", color=0x00ff00)
     embed.set_footer(text="$--------$")
     embed.set_author(name="fsociety v.1.0 by thomas1o")
-    embed.add_field(name="Powered by", value="Heroku", inline=True)
+    embed.add_field(name="Powered by:", value="Heroku", inline=True)
     await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def help(ctx):
+    author = ctx.message.author
+    embed.set_author(name="All Commands")
+    embed.add_field(name=ping , value='Sends you an Ip' ,  inline=false)
+    embed.add_field(name=info , value='Displays detailed info of your chosen Target' , inline=false)
+    embed.add_field(name=serverinfo , value='Displays detailed info on the server' , inline=false)
+    embed.add_field(name=botinfo , value='Displays detailed info on this Bot' , inline=false)
+    await bot.send_message(author, embed=embed)
 
 bot.run(os.getenv('TOKEN'))
